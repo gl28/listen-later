a1a2dccc607339b46ba3c306862461a1f7c5b9d5
gl28 <gabehladd@gmail.com>
2021-12-01 14:38:50 -0800

    added content length to RSS

--- utils/rss.go
+++ utils/rss.go
@@ -2,10 +2,19 @@ package utils
 
 import (
 	"fmt"
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/eduncan911/podcast"
 	"github.com/gl28/listen-later/models"
 )
+
+var client = &http.Client{Timeout: 10 * time.Second}
+
+type headResponse struct {
+	ContentLength string `json:"Content-Length"`
+}
 
 func CreateFeedForUser(userId int) (*podcast.Podcast, error) {
 	articles, err := models.GetArticlesForUser(userId)
@@ -47,8 +56,39 @@ func CreateFeedForUser(userId int) (*podcast.Podcast, error) {
 			Description: description,
 			PubDate: &article.DateAdded,
 		}
-		// add content length to enclosure
-		item.AddEnclosure(article.AudioURL, podcast.MP3, 1)
+
+		contentLength := article.ContentLength
+		
+		if contentLength == 0 {
+			fmt.Println("it was zero")
+			// do a HEAD request to get the audio file's length in bytes
+			req, err := http.NewRequest("HEAD", article.AudioURL, nil)
+			if err != nil {
+				return nil, err
+			}
+			resp, err := client.Do(req)
+			if err != nil {
+				return nil, err
+			}
+
+			if resp.StatusCode == 200 {
+				contentLengthString := resp.Header.Values("Content-Length")[0]
+				contentLength, _ = strconv.ParseInt(contentLengthString, 10, 64)
+	
+				// save content length so we don't have to do request next time
+				err = article.UpdateContentLength(contentLength)
+				if err != nil {
+					return nil, err
+				}
+			} else {
+				// if the HEAD request failed, there is likely a permissions issue
+				// so skip this item
+				continue
+			}
+
+		}
+
+		item.AddEnclosure(article.AudioURL, podcast.MP3, contentLength)
 		if _, err := p.AddItem(item); err != nil {
 			return nil, err
 		}
