b96be26a63ea0e1d4abf92011cd453fe8a12f695
gl28 <gabehladd@gmail.com>
2021-12-06 09:27:09 -0800

    added bootstrap and restructured index page

--- utils/templates.go
+++ utils/templates.go
@@ -3,12 +3,15 @@ package utils
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/gl28/listen-later/models"
 )
 
 var templates *template.Template
 
 type IndexContent struct {
 	FeedURL string
+	Articles []*models.Article
 }
 
 func LoadTemplates(pattern string) {
